055aedf5e09ee21b207ad6b549b19581f458a298
渡邉空馬 <k022g0093@g.neec.ac.jp>
2023-08-21 00:55:01 +0900

    04_04

--- shader/Object3d.PS.hlsl
+++ shader/Object3d.PS.hlsl
@@ -5,18 +5,23 @@ Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 
-PixelShaderOutput main(VertexShaderOutput input) {
-	PixelShaderOutput output;
-	float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
 
-	output.color = gMaterial.color * textureColor;
+    output.color = gMaterial.color * textureColor;
 
-	if (gMaterial.enableLightning != 0) {
-		float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
-		output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
-	}
-	else {
-		output.color = gMaterial.color * textureColor;
-	}
-	return output;
+    if (gMaterial.enableLightning != 0)
+    {
+        float Ndotl = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(Ndotl * 0.5f + 0.5f, 2.0f);
+
+        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    }
+    else
+    {
+        output.color = gMaterial.color * textureColor;
+    }
+    return output;
 }
